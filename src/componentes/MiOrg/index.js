72690c59d721b29479e6b5afa06fb92b685544d8
sergiolancheros <sdlb1993@gmail.com>
2023-04-17 20:04:05 -0500

    proyecto completo

---
+++ src/componentes/MiOrg/index.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import "./MiOrg.css"
+
+const MiOrg = (props)=>{
+
+    // const [nombreVariabale,functionActualiza] = useState(valorInicial)
+    //const [mostrar,actualizarMostrar] = useState(true)
+
+    //const manejarClick = ()=>{
+    //     actualizarMostrar(!mostrar)
+    // }
+
+    return <section className="orgSection">
+        <h3 className="title">Mi organización</h3>
+        <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar} />
+    </section>
+}
+
+export default MiOrg
